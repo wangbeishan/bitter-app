6eff08d584c74df72a7d6c52055c0c41978554d9
wangbeishan <wangbeishan@Beishans-MBP.lan>
2023-05-20 17:26:13 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: process.env.URL || 'http://tmthosting.wangbeishan.top',
+    baseURL: process.env.URL || 'https://tmthosting.wangbeishan.top',
     Headers: {
         'Content-Type': 'application/json'
     }
